aec517985b74f1ddc029d3f6de6d1c473a191dfa
beerpiss <lacvtg.a1.2023@gmail.com>
2023-05-13 13:26:20 +0700

    new multisrc: magapoke
    includes K MANGA and Shonen Magazine Pocket

---
+++ multisrc/overrides/magapoke/kmanga/src/KManga.kt
@@ -0,0 +1,7 @@
+package eu.kanade.tachiyomi.extension.en.kmanga
+
+import eu.kanade.tachiyomi.multisrc.magapoke.Magapoke
+
+class KManga : Magapoke("K Manga", "https://api.kmanga.kodansha.com", "en") {
+    override val version = "5.3.0"
+}
