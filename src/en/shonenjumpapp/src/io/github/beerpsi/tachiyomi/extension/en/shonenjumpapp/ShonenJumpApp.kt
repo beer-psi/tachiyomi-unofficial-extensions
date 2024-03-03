afc2832244b3c86cfdd3b98451a6f12654660ec0
beerpiss <lacvtg.a1.2023@gmail.com>
2024-03-04 02:27:58 +0700

    bruh

--- src/en/shonenjumpapp/src/io/github/beerpsi/tachiyomi/extension/en/shonenjumpapp/ShonenJumpApp.kt
+++ src/en/shonenjumpapp/src/io/github/beerpsi/tachiyomi/extension/en/shonenjumpapp/ShonenJumpApp.kt
@@ -2,6 +2,7 @@ package io.github.beerpsi.tachiyomi.extension.en.shonenjumpapp
 
 import io.github.beerpsi.tachiyomi.multisrc.viz.Viz
 
+@Suppress("MagicNumber")
 class ShonenJumpApp : Viz("Shonen Jump (App)", "https://www.viz.com/shonenjump", "en") {
 
     override val androidAppPackageName = "com.viz.wsj.android"
